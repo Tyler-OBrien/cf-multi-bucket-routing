770869a40730d8f59c7cbe9026253e9a79385bc2
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2024-02-18 16:22:11 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import regions from "./routing.json";
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+}
+
+const SUPPORTED_REGIONS = ['enam', 'wnam', 'weur', 'eeur', 'apac']
+
+
+export default {
+	getRegion(iata: regions, continent: string | undefined) {
+		const tryGetRegion = regions?.results[iata];
+		if (tryGetRegion && SUPPORTED_REGIONS.includes(tryGetRegion))
+			return tryGetRegion;
+
+		// ok we're falling back. Let's use continent and flatten it:
+		// This isn't perfect, and is partially based on what I've noticed. I think East  US / West EU is a better fallback for those. For Africa going to  WEUR and South America going to ENAM, this matches DO routing behavior.
+		switch (continent) {
+			case "NA":
+			case "T1":  // Tor is slow as heck anyway, they won't notice. This is the easiest for a fallback anyway.
+				return 'enam';
+			case "AF":
+			case "EU":
+				return 'weur';
+			case "SA":
+			case "AN":
+				return 'enam';
+			case "OC":
+			case "AS":
+				return 'apac';
+		}
+
+		return SUPPORTED_REGIONS[0]; // customize fallback
+	},
+	async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+
+		const url = new URL(req.url);
+		const key = url.pathname.slice(1);
+
+		var getBucketLocation = this.getRegion(req.cf?.colo, req.cf?.continent);
+		const object = await ((env[getBucketLocation] as R2Bucket).get(key));
+
+		
+        if (object === null) {
+			return new Response('Object Not Found', { status: 404, headers: {
+				"region": getBucketLocation
+			} });
+		  }
+  
+		  const headers = new Headers();
+		  object.writeHttpMetadata(headers);
+		  headers.set('etag', object.httpEtag);
+		  headers.set('region', getBucketLocation)
+  
+		  return new Response(object.body, {
+			headers,
+		  });
+	},
+};
