c5f1b4c7101c4131b1bc968ac2284ad0236af17c
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2024-08-07 23:20:57 -0400

    Some clean up and return why we routed you to a location in header

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import regions from "./routing.json";
+import regions from './routing.json';
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -14,72 +14,84 @@ export interface Env {
 	// MY_SERVICE: Fetcher;
 }
 
-const SUPPORTED_REGIONS = ['enam', 'wnam', 'weur', 'eeur', 'apac']
+class GetRegionResult {
+	constructor(region: string, routing: string) {
+		this.region = region;
+		this.routing = routing;
+	}
+	region: string;
+	routing: string;
+}
 
+// Customize with your bucket bindings, we're assuming each binding bucket is 1 to 1 named with each region. See my wrangler_toml for more info
+const SUPPORTED_REGIONS = ['enam', 'wnam', 'weur', 'eeur', 'apac'];
 
 export default {
-	getRegion(iata: regions, continent: string | undefined) {
-		const tryGetRegion = regions?.results[iata];
-		if (tryGetRegion)
-		{
-			if (SUPPORTED_REGIONS.includes(tryGetRegion))
-				return tryGetRegion;
-			else 
-				console.log(`Fallback, we tried to use ${tryGetRegion}, but it wasn't part of SUPPORTED_REGIONS, for ${iata}.`) 
-			//  This code was designed so you have buckets in all locations. Kind of hard to make a smart fallback as well based on new regions we don't know the name of yet.
-			//  The continent switching below should be enough, and would  also have to be updated  for new locations
-		}
-
-		console.log(`Fallback, couldn't find anything for ${iata}`)
-		// ok we're falling back. Let's use continent and flatten it:
-		// This isn't perfect, and is partially based on what I've noticed. I think East  US / West EU is a better fallback for those. For Africa going to  WEUR and South America going to ENAM, this matches DO routing behavior.
-		switch (continent) {
-			case "NA":
-			case "T1":  // Tor is slow as heck anyway, they won't notice. This is the easiest for a fallback anyway.
-				return 'enam';
-			case "AF":
-			case "EU":
-				return 'weur';
-			case "SA":
-			case "AN":
-				return 'enam';
-			case "OC":
-			case "AS":
-				return 'apac';
-		}
-
-		return SUPPORTED_REGIONS[0]; // simple fallback
-	},
 	async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-
 		const url = new URL(req.url);
 		const key = url.pathname.slice(1);
-		let getBucketLocation = null;
-		if (url.searchParams.has("region")) {
-			let getRegionOverride = url.searchParams.get("region");
+		let getBucketLocation: GetRegionResult | null = null;
+		if (url.searchParams.has('region')) {
+			let getRegionOverride = url.searchParams.get('region');
 			if (getRegionOverride && SUPPORTED_REGIONS.includes(getRegionOverride))
-				getBucketLocation = getRegionOverride;
+				getBucketLocation = new GetRegionResult(getRegionOverride, 'override');
 		}
-		if (getBucketLocation == null)
-			getBucketLocation = this.getRegion(req.cf?.colo, req.cf?.continent);
-		const object = await ((env[getBucketLocation] as R2Bucket).get(key));
 
+
+		if (getBucketLocation == null) getBucketLocation = getRegion(req.cf?.colo, req.cf?.continent);
+
+		const object = await (env[getBucketLocation.region] as R2Bucket).get(key);
 
 		if (object === null) {
 			return new Response('Object Not Found', {
-				status: 404, headers: {
-					"region": getBucketLocation
-				}
+				status: 404,
+				headers: {
+					"region": getBucketLocation.region,
+					"routing": getBucketLocation.routing,				},
 			});
 		}
 
 		const headers = new Headers();
 		object.writeHttpMetadata(headers);
 		headers.set('etag', object.httpEtag);
-		headers.set('region', getBucketLocation)
+		headers.set('routing', getBucketLocation.region);
+		headers.set('region', getBucketLocation.region);
 
 		return new Response(object.body, {
 			headers,
 		});
 	},
+};
+
+const getRegion = function (iata: regions, continent: string | undefined): GetRegionResult {
+	const tryGetRegion = regions?.results[iata];
+	if (tryGetRegion) {
+		if (SUPPORTED_REGIONS.includes(tryGetRegion)) return new GetRegionResult(tryGetRegion, 'map');
+		else console.log(`Fallback, we tried to use ${tryGetRegion}, but it wasn't part of SUPPORTED_REGIONS, for ${iata}.`);
+		//  This code was designed so you have buckets in all locations. Kind of hard to make a smart fallback as well based on new regions we don't know the name of yet.
+		//  The continent switching below should be enough, and would  also have to be updated  for new locations
+	}
+
+	console.log(`Fallback, couldn't find anything for ${iata}`);
+
+	if (!continent) return new GetRegionResult(SUPPORTED_REGIONS[0], 'fallback-global'); // simple fallback
+
+	// ok we're falling back. Let's use continent and flatten it:
+	// This isn't perfect, and is partially based on what I've noticed. I think East  US / West EU is a better fallback for those. For Africa going to  WEUR and South America going to ENAM, this matches DO routing behavior.
+	switch (continent) {
+		case 'NA':
+		case 'T1': // Tor is slow as heck anyway, they won't notice. This is the easiest for a fallback anyway.
+			return new GetRegionResult('enam', 'fallback-continent');
+		case 'AF':
+		case 'EU':
+			return new GetRegionResult('weur', 'fallback-continent');
+		case 'SA':
+		case 'AN':
+			return new GetRegionResult('enam', 'fallback-continent');
+		case 'OC':
+		case 'AS':
+			return new GetRegionResult('apac', 'fallback-continent');
+	}
+
+	return new GetRegionResult(SUPPORTED_REGIONS[0], 'fallback-global'); // simple fallback
 };
